fe3eb28c7cc8ef48a222ff5f25a80f2003316c26
atingey <54607592+atingey@users.noreply.github.com>
2020-03-13 14:38:47 -0600

    this is the way...

--- src/Client.cpp
+++ src/Client.cpp
@@ -20,25 +20,99 @@
 Client::~Client() {
 }
 
-
+/**
+ * Performs the initialization functions that normally would be in a constructor, but needs to be a separate
+ * function for googletest so clients may be initialized in the SetUp() function.
+ * Creates player_#.action_board.json.
+ * @param player - the id of the player
+ * @param board_size - the square size of the action board
+ */
 void Client::initialize(unsigned int player, unsigned int board_size){
+    this->player = player;
+    this->board_size = board_size;
+    board_name = "player_"+to_string(this->player)+".action_board.json";
+
+    vector< vector< int >> bod;
+    vector< int > line(board_size, 0);
+    for(int i=0; i<board_size; i++) {
+        bod.push_back(line);
+    }
+
+    remove(board_name.c_str());
+    ofstream ofp(board_name);
+
+    cereal::JSONOutputArchive write_archive(ofp);
+    write_archive(cereal::make_nvp("board", bod));
 }
 
-
+/**
+ * Fires a shot on the coordinate target and creates a player_#.shot.json file
+ * @param x - coordinate
+ * @param y - coordinate
+ */
 void Client::fire(unsigned int x, unsigned int y) {
+    string shoot = "player_"+to_string(this->player)+".shot.json";
+    remove(shoot.c_str());
+
+    ofstream ofp(shoot);
+    cereal::JSONOutputArchive write_archive(ofp);
+    write_archive(cereal::make_nvp("x", x), cereal::make_nvp("y", y));
 }
 
-
+/**
+ * Checks if a result file is available for
+ * @return true if result is available, false otherwise
+ */
 bool Client::result_available() {
+    string checkme = "player_"+to_string(this->player)+".result.json";
+    ifstream ifp(checkme);
+    return ifp.good();
+
 }
 
-
+/**
+ * Gets the result from the player_#.result.json
+ * @return the result as either HIT, MISS, or OUT_OF_BOUNDS
+ */
 int Client::get_result() {
+    int end;
+    string b = "player_"+to_string(player)+".result.json";
+    ifstream inFile(b);
+    cereal:: JSONInputArchive read_archive(inFile);
+    read_archive(end);
+    inFile.close();
+    remove(b.c_str());
+
+    if(end > 1 || end < -1) {
+        throw invalid_argument("NOT IT");
+    }
+    return end;
 }
 
 
-
+/**
+ * Updates the internal representation of player_#.action_board.json on the result of a shot.
+ * @param result - the result returned from the server
+ * @param x - coordinate
+ * @param y - coordinate
+ */
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+
+
+    string file = "player_"+to_string(this->player)+".action_board.json";
+    ifstream fml(file);
+    cereal::JSONInputArchive read_archive(fml);
+
+    vector< vector<int>> boa;
+    read_archive(boa);
+    fml.close();
+    remove(file.c_str());
+    boa[y][x] = result;
+
+    ofstream os(file);
+    cereal::JSONOutputArchive write_archive(os);
+    write_archive(cereal::make_nvp("board", boa));
+
 }
 
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -17,6 +17,7 @@
 
 #include "common.hpp"
 #include "Server.hpp"
+#include <iostream>
 
 
 /**
@@ -26,19 +27,163 @@
  * @return length of the file in bytes
  */
 int get_file_length(ifstream *file){
+//    std:: streampos fsize = 0;
+//    fsize = file->tellg();
+//    file->seekg(0, std::ios::end);
+//    fsize = file->tellg() - fsize;
+//    file->close();
+//    return fsize;
 }
 
-
+/**
+* Performs Server initialization
+*
+* Need to do initialization outside of the constructor, so that the object may be initialized inside the
+* googletest SetUp() method.
+* The method opens player setup boards containing ship positions and checks the size of boards compared to
+* board_size parameter.
+* @param board_size - the size of a square board
+* @param p1_setup_board - file name of player 1's board
+* @param p2_setup_board - file name of player 2's board
+*/
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+
+    if (p1_setup_board== "" || p2_setup_board== "") {
+        throw ServerException("Empty board files!!!");
+    }
+
+
+    ifstream inf1;
+    string f1line;
+
+    inf1.open("player_1.setup_board.txt");
+
+    while (getline(inf1, f1line)) {
+        //        inf1 >> f1line;
+        if (f1line.length() != board_size) {
+            throw ServerException("Wrong Board Size!!!");
+        } else {
+            this->board_size = board_size;
+        }
+    }
+
+    ifstream inf2;
+    string f2line;
+    inf2.open("player_1.setup_board.txt");
+    while (getline(inf2, f2line)) {
+        //        inf1 >> f1line;
+        if (f2line.length() != board_size) {
+            throw ServerException("Wrong Board Size!!!");
+            }
+        else {
+            this->board_size = board_size;
+        }
+    }
 }
 
-
+/**
+* Checks the coordinates of a shot against setup board of player
+*
+* Check that player number within bounds, checks that shot coordinates within bounds, determines if the shot
+* results in a HIT, or a MISS.
+* @param player - player number
+* @param x - coordinate
+* @param y - coordinate
+* @return returns shot result as either HIT, MISS, or OUT_OF_BOUNDS
+*/
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+
+    if (player < 1 || player > 2) {
+        throw ServerException("Wrong player number!!");
+    }
+    if (x < 0 || x > board_size - 1 || y < 0 || y > board_size - 1) {
+        return OUT_OF_BOUNDS;
+    }
+
+    ifstream inf;
+    if (player == 1) {
+        inf.open("player_2.setup_board.txt");
+    }
+    if (player ==2) {
+        inf.open("player_1.setup_board.txt");
+    }
+    vector< vector< char>> boa(board_size, vector<char>(board_size));
+
+    while(!inf.eof()) {
+        for (int b=0; b<board_size; b++) {
+            for (int a=0; a<board_size; a++) {
+                inf >> boa[b][a];
+            }
+        }
+        if (boa[x][y] == '_') {
+            return MISS;
+        }
+        else {
+            return HIT;
+        }
+    }
+//  check layout of vector "boa"
+//    for (int i = 0; i<board_size; i++) {
+//        for (int j = 0; j<board_size; j++) {
+//            cout << boa[i][j] << ' ';
+//        }
+//        cout << endl;
+//    }
+//    cout<< boa[x][y];
+//    cout<< endl;
+
+    }
+
+/**
+ * Processes a shot issued by player
+ *
+ * Gets the shot from player, extracts coordinates, passes the information of evaluate_shot, and writes the result
+ * into player_#.result.json.
+ * @param player - player number
+ * @return returns SHOT_PROCESSED, or NO_SHOT_FILE if nothing to process
+ */
+    int Server::process_shot(unsigned int player) {
+
+    if (player < 1 || player > 2) {
+        throw ServerException("Wrong player number!!");
+    }
+
+    int x, y;
+    string first_board = "player_" + to_string(player) + ".shot.json";
+    string result_board = "player_" + to_string(player) + ".result.json";
+
+    ifstream inFile(first_board);
+
+    if (!inFile) {
+        return NO_SHOT_FILE;
+    } else if (inFile.good()) {
+
+        cereal::JSONInputArchive read_archive(inFile);
+        read_archive(x, y);
+        inFile.close();
+//        printf("%d%d", x, y);
+
+        remove(first_board.c_str());
+
+        int result = evaluate_shot(player, y, x);
+//            cout << result;
+//            cout << endl;
+
+        remove(result_board.c_str());
+
+        ofstream ofp(result_board);
+        cereal::JSONOutputArchive write_archive(ofp);
+
+        if (x < 0 || x > board_size - 1 || y < 0 || y > board_size - 1) {
+            result = 0;
+            write_archive(CEREAL_NVP(result));
+            return 0;
+        }
+
+        write_archive(CEREAL_NVP(result));
+        return SHOT_FILE_PROCESSED;
+    }
 }
 
-
-int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
-}
